5876975e9a7fbfbf571210e15b533993fe80f504
Lucas <contatolucasoficial0@gmail.com>
2024-03-21 20:27:30 -0300

    tests

---
+++ src/__tests__/loginTest.test.ts
@@ -0,0 +1,6 @@
+import * as service from "../services/loginService.js";
+import * as route from "../routes/loginRoute.js";
+import {describe} from "node:test";
+
+describe();
+
---
+++ src/__tests__/noteTest.test.ts
@@ -0,0 +1,5 @@
+import * as service from "../services/noteService.js";
+import * as route from "../routes/noteRoute.js";
+import {describe} from "node:test";
+
+describe("");
