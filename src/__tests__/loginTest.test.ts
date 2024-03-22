c762100594894218dafc0c55b7644dbabd64aa1d
Lucas <contatolucasoficial0@gmail.com>
2024-03-21 21:27:54 -0300

    test

--- src/__tests__/loginTest.test.ts
+++ src/__tests__/loginTest.test.ts
@@ -1,6 +1,23 @@
 import * as service from "../services/loginService.js";
 import * as route from "../routes/loginRoute.js";
 import {describe} from "node:test";
+import request from 'supertest';
+import app from '../app.js';
 
-describe();
+describe('Login Service Tests', () => {
+    app.use(route.default);
+    it('Test findUser function', async () => {
+        const result = await service.findUser('testUser', 'testPassword');
+        expect(result).toBeTruthy();
+    });
 
+});
+
+describe('Login Route Tests', () => {
+    app.use(route.default);
+    test('GET /login endpoint', async () => {
+        const response = await request(app).get('/login');
+        expect(response.status).toBe(200);
+    });
+
+});
